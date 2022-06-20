42be6a786b921b844ee5cb7885b525c09d225d63
Gurpreet Bhopal <gbhopal91@gmail.com>
2022-06-20 12:18:27 +0100

    updated url

--- apollo-client.js
+++ apollo-client.js
@@ -1,10 +1,10 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-    uri: 'https://bjgjo43suvfr3at5bvsp4hxegi.appsync-api.us-east-1.amazonaws.com/graphql',
+    uri: 'https://h6lvdmjlojbvveexmz4o3hwdhi.appsync-api.us-east-1.amazonaws.com/graphql',
     cache: new InMemoryCache(),
     headers: {
-        'x-api-key': 'da2-lnsrz3lckrhclj5v2jwsx4g56i',
+        'x-api-key': 'da2-5eroj2zwyncunjssw4ykzv33ha',
     }
 });
 
