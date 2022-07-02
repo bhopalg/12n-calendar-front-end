260230df6a6afb36f21992bee6eb8a237926452b
Gurpreet Bhopal <gbhopal91@gmail.com>
2022-07-02 13:11:41 +0100

    updated env

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_MAILCHIMP_URL: process.env.NEXT_PUBLIC_MAILCHIMP_URL,
+    NEXT_MAILCHIMP_API_KEY: process.env.NEXT_MAILCHIMP_API_KEY,
+    NEXT_MAILCHIMP_API_SERVER: process.env.NEXT_MAILCHIMP_API_SERVER,
+    NEXT_MAILCHIMP_AUDIENCE_ID: process.env.NEXT_MAILCHIMP_AUDIENCE_ID,
+  }
 }
 
 module.exports = nextConfig
